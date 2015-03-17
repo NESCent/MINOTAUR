f27eae3c815545558983195d1bba7da90809709d
Bob Verity <r.verity@imperial.ac.uk>
2015-03-17 16:42:05 -0400

    Skeleton framework of final Rshiny app

---
+++ inst/misc/ShinyApp_version1.0/Deploy.R
@@ -0,0 +1,30 @@
+
+# Deploy.R (one of three scripts required to run any Rshiny app)
+
+# Author: Bob Verity
+# Date: 17/03/2015
+
+# Purpose:
+# Simply points to the folder that the other Rshiny files are stored in, and executes these files.
+
+#### ------------------------------------------------------------------
+
+# Choose Shiny path based on user name
+userName = 'Bob'
+
+if (userName=='Bob') {
+  shinyPath = '/Users/Bob/Dropbox/Bob/Work/Side Projects/MANIPULATE/inst/misc/ShinyApp_version1.0'
+}
+
+# Load packages
+require('shiny')
+require('devtools')
+require('rCharts')
+
+# maybe needed?
+#install_github('rCharts','ramnathv')
+#install_github('rHighcharts','metagraf')
+
+# run Shiny app
+runApp(shinyPath)
+
