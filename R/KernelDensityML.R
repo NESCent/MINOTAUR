dbaa3bd22c17322fc06737bc1207f5b494a948d7
Bob Verity <r.verity@imperial.ac.uk>
2015-03-18 18:50:00 -0400

    ML kernel density ready for testing

--- R/KernelDensityML.R
+++ R/KernelDensityML.R
@@ -1,48 +1,66 @@
 
-
 #'  R Verity
 #'  March 18, 2015
 #'  calculate density of point i from all other points
-#'  @param df is ...
-#'  @param i is ...
-#'  @param lambda is ...
+#'  @param df is a dataframe containing observations in rows and statistics in columns
+#'  @param i is the row number that will be compared against all other rows
+#'  @param sigma is the standard deviation of the normal kernel in all dimensions
 #'  @author R Verity
 #'  @keywords internal
 
-
-## .logLike_i ##
+## .logLike_internal ##
 # calculate density of point i from all other points
-.logLike_i = function(df,i,lambda) {
-  rawProbs = rowSums(apply(df,2,function(x){dnorm(x[i],x[-i],sd=lambda,log=TRUE)}))
+.logLike_internal = function(df,i,sigma) {
+  rawProbs = rep(0,nrow(df)-1)
+  for (j in 1:ncol(df)) {
+    rawProbs = rawProbs + dnorm(df[i,j],df[-i,j],sd=sigma[j],log=TRUE)
+  }
   totalProb = log(mean(exp(rawProbs)))
   return(totalProb)
-} # end .logLike_i
-
+} # end .logLike_internal
 
 
 #'  R Verity
 #'  March 18, 2015
-#'  calculate deviance (-2*logLikelihood) of all points in data frame
-#'  @param df is ...
-#'  @param lambda is ...
+#'  calculate kernel density of target point given anchor points
+#'  @param target is a vector of points on which the density will be calculated
+#'  @param anchor is a data frame of points used to produce kernel density
+#'  @param sigma is the standard deviation of the normal kernel in all dimensions
 #'  @author R Verity
 #'  @keywords internal
 
-## .deviance_all ##
-# calculate deviance (-2*logLikelihood) of all points in data frame
-.deviance_all = function(df,lambda) {
-  -2*sum(mapply(.logLike_i,i=1:nrow(df),MoreArgs=list(df=df,lambda=lambda)))
-} # end .deviance_all
-
-X = data.frame(t1=rnorm(100),t2=rnorm(100))
-
-.deviance_all(X,0.1)
-
+## .logLike_external ##
+# calculate kernel density of target point given anchor points
+.logLike_external = function(target,anchor,sigma) {
+  rawProbs = rep(0,nrow(anchor))
+  for (j in 1:length(target)) {
+    rawProbs = rawProbs + dnorm(target[j],anchor[,j],sd=sigma[j],log=TRUE)
+  }
+  totalProb = log(mean(exp(rawProbs)))
+  return(totalProb)
+} # end .logLike_external
 
 
 #'  R Verity
 #'  March 18, 2015
-#'  Kernel density with maximum likelihood bandwidth selection
+#'  calculate deviance (-2*logLikelihood) of all points in data frame based on the internal kernel density
+#'  @param df is a dataframe containing observations in rows and statistics in columns
+#'  @param lambda is a scaling factor on the bandwidth (final bandwidth = lambda*sigma)
+#'  @param sigma is the standard deviation of the normal kernel in all dimensions
+#'  @author R Verity
+#'  @keywords internal
+
+## .deviance_internal ##
+# calculate deviance (-2*logLikelihood) of all points in data frame based on the internal kernel density
+.deviance_internal = function(df,lambda,sigma) {
+  dev <- -2*sum(mapply(.logLike_internal,i=1:nrow(df),MoreArgs=list(df=df,sigma=lambda*sigma)))
+  return(dev)
+}
+
+
+#'  R Verity
+#'  March 18, 2015
+#'  calculate kernel density distance with maximum likelihood bandwidth selection
 #'  @rdname KernelDensityML
 #'  @name KernelDensityML
 #'  @param dfv is a dataframe containing the observations in rows and the statistics in columns
@@ -53,28 +71,44 @@ X = data.frame(t1=rnorm(100),t2=rnorm(100))
 #'  ##TO DO
 
 ## KernelDensityML ##
+# calculate kernel density distance with maximum likelihood bandwidth selection
 KernelDensityML <- function(dfv, column.nums){
   # This function produces a kernel density from the selected points, and returns the kernel 'distance'
   # as -2*logLikelihood of all points under this model. Kernel bandwidth is chosen automatically by
   # maximum likelihood.
   # Nb. In theory can work with missing data, but not coded up yet
   
-  # subset data
-  df.vars <- dfv[,column.nums]
+  # subset and subsample data
+  df.vars <- dfv[,column.nums,drop=FALSE]
+  nvars <- length(column.nums)
   nlocs <- nrow(df.vars)
+  df.subSample <- df.vars[sample(nlocs,min(200,nlocs)),,drop=FALSE]
   
+  # calculate standard deviation of each variable
+  sigma = apply(df.vars,2,function(x){sd(x)})
   
+  # estimate optimal bandwidth by maximum likelihood
+  optim_results <- optim(par=1,.deviance_internal,df=df.subSample,sigma=sigma,method='Brent',lower=0,upper=10)
+  ml_bandwidth <- optim_results$par*sigma
   
-#   D <- rep(NA, nlocs) # Mahalanobis distance
-#   for (i in 1:nrow(df.vars)){
-#     if(sum(is.na(df.vars[i,]))==0){  # The locus has to have an observation for each test statistic
-#       x <- t(df.vars[i,])
-#       diff <- x-mu
-#       D[i] <- sqrt( t(diff) %*% solve(S) %*% (diff) )
-#     }else{ # or else NA
-#       D[i]=NA
-#     }
-#   }
-#   return(D)
+  # calculate deviance under optimal bandwidth
+  kernelDist <- -2*apply(df.vars,1,function(x){.logLike_external(x,df.subSample,ml_bandwidth)})
+  
+  return(kernelDist)
 } # end KernelDensityML
 
+
+n = 1000
+d = 10
+X1 = rmvnorm(n/2,mean=rnorm(d),sigma=diag(d)+(1-diag(d))*0.1)
+X2 = rmvnorm(n/2,mean=rnorm(d),sigma=diag(d)+(1-diag(d))*-0.1)
+X = rbind(X1,X2)
+X[,1] = X[,1]/10
+plot(X[,1],X[,2])
+
+kernelDist <- KernelDensityML(dfv=X,column.nums=1:ncol(X))
+
+rankOrder = order(kernelDist,decreasing=TRUE)
+colVec = rep('grey',length(kernelDist))
+colVec[rankOrder][1:10] = 'red'
+plot(X[,1],X[,2],col=colVec)
