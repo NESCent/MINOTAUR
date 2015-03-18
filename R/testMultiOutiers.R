baca551e3946005afcf85367715b76236a7ef6c8
Katie Lotterhos <lotterke@wfu.edu>
2015-03-18 17:13:51 -0400

    Distance measures in one script, started tests
    All distance measures were moved into DistanceFunctions.R
    started testing them with test/MultiOutliers.R

---
+++ R/testMultiOutiers.R
@@ -0,0 +1,34 @@
+
+getwd()
+source("R/DistanceFunctions.R")
+
+  dfv <- read.table("inst/misc/OneRefSim.txt", header=TRUE)
+  dfv2 <- dfv[dfv$SNPIncluded,]
+  colnums <- c(11, 12, 16)
+  loci.ind <- 9500:9996
+
+  head(dfv)
+  str(dfv)
+  cbind(colnames(dfv))
+  dim(dfv)
+  dim(dfv2)
+  table(dfv2$s_high)
+
+  ### Mahalanobis distance ######
+  Md <- Mahalanobis(dfv2, colnums)
+  str(Md)
+
+  plot(Md[[3]][loci.ind], col=factor(dfv$s_high[loci.ind]))
+
+  ### KernelDensSD ######
+  Kd <- KernelDensSD(dfv2, colnums, 3)
+  str(Kd)
+  plot(Kd[[3]][loci.ind], col=factor(dfv$s_high[loci.ind]))
+
+  ### Hclust ######
+  Hcd <- hclust.ranking(dfv2, colnums)
+  str(Hcd[[1]])
+  str(Hcd[[2]])
+  plot(Hcd$rank.outliers)
+  plot(Hcd[[1]][loci.ind])
+  plot(Hcd[[2]][loci.ind], col=factor(dfv$s_high[loci.ind]))
