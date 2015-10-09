e79462fa0b2da70b7f04d0635ab819515727a59c
liuyang wang <wallacewly@gmail.com>
2015-10-08 21:37:59 -0400

    modification

--- inst/mainserver/server_Manhattanplot.R
+++ inst/mainserver/server_Manhattanplot.R
@@ -7,10 +7,8 @@ output$select_linearMH <- renderUI({
 
 # linenar Manhattan Plot
 output$LinearMHTplot <- renderPlot({
-    #yname = NULL
     yname = input$yaxis
-   
-    print(yname)
+    
     if(!is.null(rv$subData) && !is.null(yname)){
       mhtplot(mydata=rv$subData, pcut.outlier=0.002, statistic.plot = yname)
     }
