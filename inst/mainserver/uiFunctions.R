e360437da819be06cb5c73d961f0c4e1a1bdfe22
caitiecollins <caitiecollins@gmail.com>
2016-01-28 16:58:24 +0000

    changed ui xycol selection to pick numeric columns

--- inst/mainserver/uiFunctions.R
+++ inst/mainserver/uiFunctions.R
@@ -8,11 +8,19 @@
 .getxSelection <- function(mainData){
   out <- NULL
   if(!is.null(mainData)){
+    
+    sel <- NULL
+    ## get numeric variables
+    numCols <- which(sapply(c(1:ncol(mainData)), function(e) is.numeric(mainData[,e])))
+    if(length(numCols) > 0){
+      sel <- names(mainData)[numCols[1]]
+    }
+    
     out <- selectInput(
       inputId = "xSelection",
       label = "Choose x-axis data:",
       choices = names(mainData),
-      selected = names(mainData)[4] #will have to update with publishable dataset
+      selected = sel #may want to update with publishable dataset
     )
   }
   return(out)
@@ -23,11 +31,21 @@
 .getySelection <- function(mainData){
   out <- NULL
   if(!is.null(mainData)){
+    sel <- NULL
+    ## get numeric variables
+    numCols <- which(sapply(c(1:ncol(mainData)), function(e) is.numeric(mainData[,e])))
+    if(length(numCols) > 0){
+      sel <- names(mainData)[numCols[1]]
+      if(length(numCols) >= 2){
+        sel <- names(mainData)[numCols[2]]
+      }
+    }
+    
     out <- selectInput(
       inputId = "ySelection",
       label = "Choose y-axis data:",
       choices = names(mainData),
-      selected = names(mainData)[5] #will have to update with publishable dataset
+      selected = sel #may want to update with publishable dataset
     )
   }
   return(out)
@@ -50,11 +68,22 @@
 .getColVarSelection <- function(mainData){
   out <- NULL
   if(!is.null(mainData)){
+    
+    sel <- NULL
+    ## get numeric variables
+    numCols <- which(sapply(c(1:ncol(mainData)), function(e) is.numeric(mainData[,e])))
+    if(length(numCols) > 0){
+      sel <- names(mainData)[numCols[1]]
+      if(length(numCols) >= 3){
+        sel <- names(mainData)[numCols[3]]
+      }
+    }
+    
     out <- selectInput(
       inputId = "colVarSelection",
       label = "Choose variable to highlight Outliers for on plot:",
       choices = names(mainData),
-      selected = names(mainData)[5] #will have to update with publishable dataset
+      selected = sel #may want to update with publishable dataset
     )
   }  
   return(out)
@@ -64,12 +93,12 @@
 ## .getColPal ##
 .getColPal <- function(){
   out <- selectInput("colPal", "Choose a color pallette to use:",
-    c("SeaSun" = "seasun",
-      "Funky" = "funky",
-      "Spectral" = "spectral",
-      "Azur" = "azur",
-      "Wasp" = "wasp"
-    )
+                     c("SeaSun" = "seasun",
+                       "Funky" = "funky",
+                       "Spectral" = "spectral",
+                       "Azur" = "azur",
+                       "Wasp" = "wasp"
+                     )
   )
   return(out)
 } # end .getColPal
