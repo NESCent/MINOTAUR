1d2d02351d528ac26739c969ec105c763d789d06
Robert <rverity@dyn1214-197.wlan.ic.ac.uk>
2016-04-28 17:37:01 +0100

    Can now use custom covariance matrices

--- src/harmonicDist.cpp
+++ src/harmonicDist.cpp
@@ -7,35 +7,7 @@ using namespace std;
 //------------------------------------------------
 // calculates distance between observations as the harmomic mean distance of every point to every other.
 // [[Rcpp::export]]
-Rcpp::List C_harmonicDist(std::vector< std::vector<double> > data) {
-    
-    int dims = int(data.size());
-    int n = data[0].size();
-    
-    double z, x;
-    vector<double> distance(n);
-    
-    for (int i=0; i<n; i++) {
-        z = 0;
-        for (int j=0; j<n; j++) {
-            if (i==j)
-                continue;
-            x = 0;
-            for (int k=0; k<dims; k++) {
-                x += (data[k][i]-data[k][j])*(data[k][i]-data[k][j]);
-            }
-            z += 1.0/sqrt(x);
-        }
-        distance[i] = double(n)/z;
-    }
-    
-    // return values
-    return Rcpp::List::create(Rcpp::Named("distance")=distance);
-}
-
-//------------------------------------------------
-// [[Rcpp::export]]
-Rcpp::List C_harmonicDist2(std::vector< std::vector<double> > data, std::vector< std::vector<double> > S_inv) {
+Rcpp::List C_harmonicDist(std::vector< std::vector<double> > data, std::vector< std::vector<double> > S_inv) {
 
     int dims = int(data.size());
     int n = data[0].size();
--- src/kernelDist.cpp
+++ src/kernelDist.cpp
@@ -7,42 +7,7 @@ using namespace std;
 //------------------------------------------------
 // calculates kernel density of all points from all others.
 // [[Rcpp::export]]
-Rcpp::List C_kernelDist(std::vector< std::vector<double> > data, double sigma2) {
-
-    int dims = int(data.size());
-    int n = data[0].size();
-    double C1 = 0.5/sigma2;
-    double C2 = 2*log(double(n-1)) + dims*log(6.283185*sigma2);
-
-    double z, x;
-    vector<double> distance(n);
-
-    for (int i=0; i<n; i++) {
-        z = log(0.0);
-        for (int j=0; j<n; j++) {
-            if (i==j)
-                continue;
-            x = 0;
-            for (int k=0; k<dims; k++) {
-                x += (data[k][i]-data[k][j])*(data[k][i]-data[k][j]);
-            }
-            // the next if statement does z' = log(exp(z) + exp(-x*C1)) while avoiding underflow/overflow issues.
-            if ((-x*C1)>z) {
-                z = -x*C1 + log(1 + exp(z + x*C1));
-            } else {
-                z = z + log(1 + exp(-x*C1 - z));
-            }
-        }
-        distance[i] = C2 - 2*z;
-    }
-
-    // return values
-    return Rcpp::List::create(Rcpp::Named("distance")=distance);
-}
-
-//------------------------------------------------
-// [[Rcpp::export]]
-Rcpp::List C_kernelDist2(std::vector< std::vector<double> > data, double sigma2, std::vector< std::vector<double> > S_inv) {
+Rcpp::List C_kernelDist(std::vector< std::vector<double> > data, double sigma2, std::vector< std::vector<double> > S_inv) {
     
     int dims = int(data.size());
     int n = data[0].size();
